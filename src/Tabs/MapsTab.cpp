88c33cb4d1f94cf77230a939f053dd8a1fe8f082
Melissa Geels <spansjh@gmail.com>
2021-03-29 12:37:31 +0200

    Use tables API for Maps tab (fixes #2)

--- src/Tabs/MapsTab.cpp
+++ src/Tabs/MapsTab.cpp
@@ -31,63 +31,73 @@ void MapsTab::Render()
 
 	ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(4, 0));
 
-	for (auto& map : m_inspector->m_processRegions) {
-		ImGui::PushID(map.m_start);
-		ImGui::PushFont(Resources::FontMono);
+	if (ImGui::BeginTable("Maps", 3)) {
+		ImGui::TableSetupColumn("Range", ImGuiTableColumnFlags_WidthFixed);
+		ImGui::TableSetupColumn("Flags", ImGuiTableColumnFlags_WidthFixed);
+		ImGui::TableSetupColumn("Name", ImGuiTableColumnFlags_WidthStretch);
+		ImGui::TableHeadersRow();
 
-		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(.5f, .5f, .5f, 1));
-		ImGui::Text(POINTER_FORMAT " - " POINTER_FORMAT, map.m_start, map.m_end);
-		ImGui::PopStyleColor();
+		for (auto& map : m_inspector->m_processRegions) {
+			ImGui::TableNextRow();
 
-		ImGui::SameLine(270);
-		if (map.IsRead()) {
-			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(.5f, 1, .5f, 1));
-			ImGui::Text("R");
-			ImGui::PopStyleColor();
-		}
+			ImGui::PushID(map.m_start);
+			ImGui::PushFont(Resources::FontMono);
 
-		ImGui::SameLine(290);
-		if (map.IsWrite()) {
-			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1, 1, .5f, 1));
-			ImGui::Text("W");
-			ImGui::PopStyleColor();
-		}
-
-		ImGui::SameLine(310);
-		if (map.IsExecute()) {
-			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1, .5f, .5f, 1));
-			ImGui::Text("X");
-			ImGui::PopStyleColor();
-		}
-
-		ImGui::SameLine(330);
-		if (map.IsProtect()) {
-			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(.5f, .5f, 1, 1));
-			ImGui::Text("P");
-			ImGui::PopStyleColor();
-		}
-
-		ImGui::PopFont();
-
-		ImGui::SameLine(350);
-
-		if (map.IsExecute()) {
-			Helpers::CodeButton(m_inspector, map.m_start);
-			ImGui::SameLine();
-		}
-		Helpers::DataButton(m_inspector, map.m_start);
-
-		ImGui::SameLine();
-
-		if (map.m_path.startswith(".")) {
+			ImGui::TableSetColumnIndex(0);
 			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(.5f, .5f, .5f, 1));
-			ImGui::TextUnformatted(map.m_path);
+			ImGui::Text(POINTER_FORMAT " - " POINTER_FORMAT, map.m_start, map.m_end);
 			ImGui::PopStyleColor();
-		} else {
-			ImGui::TextUnformatted(map.m_path);
-		}
 
-		ImGui::PopID();
+			ImGui::TableSetColumnIndex(1);
+			if (map.IsRead()) {
+				ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(.5f, 1, .5f, 1));
+				ImGui::Text("R");
+				ImGui::PopStyleColor();
+			}
+
+			ImGui::SameLine(20);
+			if (map.IsWrite()) {
+				ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1, 1, .5f, 1));
+				ImGui::Text("W");
+				ImGui::PopStyleColor();
+			}
+
+			ImGui::SameLine(40);
+			if (map.IsExecute()) {
+				ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1, .5f, .5f, 1));
+				ImGui::Text("X");
+				ImGui::PopStyleColor();
+			}
+
+			ImGui::SameLine(60);
+			if (map.IsProtect()) {
+				ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(.5f, .5f, 1, 1));
+				ImGui::Text("P");
+				ImGui::PopStyleColor();
+			}
+
+			ImGui::PopFont();
+
+			ImGui::TableSetColumnIndex(2);
+			if (map.IsExecute()) {
+				Helpers::CodeButton(m_inspector, map.m_start);
+				ImGui::SameLine();
+			}
+			Helpers::DataButton(m_inspector, map.m_start);
+
+			ImGui::SameLine();
+
+			if (map.m_path.startswith(".")) {
+				ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(.5f, .5f, .5f, 1));
+				ImGui::TextUnformatted(map.m_path);
+				ImGui::PopStyleColor();
+			} else {
+				ImGui::TextUnformatted(map.m_path);
+			}
+
+			ImGui::PopID();
+		}
+		ImGui::EndTable();
 	}
 
 	ImGui::PopStyleVar();
