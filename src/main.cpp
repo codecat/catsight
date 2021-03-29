e14695dce58829d595f351f928a527b9d864a7a4
Melissa Geels <spansjh@gmail.com>
2021-03-29 17:02:06 +0200

    More testing data

--- src/main.cpp
+++ src/main.cpp
@@ -16,17 +16,30 @@ int main()
 
 #if defined(PLATFORM_LINUX)
 	const size_t testMemSize = 0x1000000;
-	void* testMem = mmap((void*)0x1000000, testMemSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED_NOREPLACE, -1, 0);
-	memset(testMem, 0x00, testMemSize);
-	for (auto p = (uintptr_t*)testMem; p < (uintptr_t*)((uintptr_t)testMem + testMemSize); p++) {
-		if (((uintptr_t)p & 0xF) == 0) {
-			*p = (uintptr_t)p;
-		}
+	uintptr_t* testMem = (uintptr_t*)mmap((void*)0x1000000, testMemSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED_NOREPLACE, -1, 0);
+	memset(testMem, 0, testMemSize);
+
+	testMem[1] = (uintptr_t)"Pointer chain to code";
+	for (int i = 2; i <= 6; i++) {
+		testMem[i] = (uintptr_t)(testMem + i + 1);
 	}
-	((void**)testMem)[1] = (void*)&do_something;
-	((void**)testMem)[3] = (void*)&main;
-	((void**)testMem)[5] = (void*)"this is a constant string";
-	((uintptr_t*)testMem)[6] = (uintptr_t)testMem + 2;
+	testMem[6] = (uintptr_t)&main;
+
+	testMem[10] = (uintptr_t)"Pointer chain to float";
+	for (int i = 10; i <= 14; i++) {
+		testMem[i] = (uintptr_t)(testMem + i + 1);
+	}
+	*(float*)(testMem + 14) = 1.23f;
+
+	testMem[17] = (uintptr_t)"Pointer to itself";
+	testMem[18] = (uintptr_t)(testMem + 18);
+
+	testMem[20] = (uintptr_t)"Floats";
+	auto floatArray = (float*)(testMem + 21);
+	floatArray[0] = 1.0f;
+	for (int i = 1; i < 5; i++) {
+		floatArray[i] = 1.0f * (i / 2.0f);
+	}
 #endif
 
 	Explorer::Instance = new Explorer();
