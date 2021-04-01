51117cd6c5f0168f29475cb03310d0144953ca6f
Melissa Geels <spansjh@gmail.com>
2021-04-01 13:59:42 +0200

    Also add test memory region on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include <Explorer.h>
 
 #if defined(PLATFORM_LINUX)
-#include <sys/mman.h>
+#  include <sys/mman.h>
+#elif defined(PLATFORM_WINDOWS)
+#  include <Windows.h>
 #endif
 
 void do_something()
@@ -14,9 +16,14 @@ int main()
 {
 	srand(time(nullptr));
 
-#if defined(PLATFORM_LINUX)
+	// Create test memory page
 	const size_t testMemSize = 0x1000000;
+#if defined(PLATFORM_LINUX)
 	uintptr_t* testMem = (uintptr_t*)mmap((void*)0x1000000, testMemSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED_NOREPLACE, -1, 0);
+#elif defined(PLATFORM_WINDOWS)
+	uintptr_t* testMem = (uintptr_t*)VirtualAlloc((void*)0x1000000, testMemSize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+#endif
+
 	memset(testMem, 0, testMemSize);
 
 	testMem[1] = (uintptr_t)"Pointer chain to code";
@@ -40,8 +47,8 @@ int main()
 	for (int i = 1; i < 5; i++) {
 		floatArray[i] = 1.0f * (i / 2.0f);
 	}
-#endif
 
+	// Initialize and run application
 	Explorer::Instance = new Explorer();
 	Explorer::Instance->Run();
 	delete Explorer::Instance;
